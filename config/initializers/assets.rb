11fd70390de9e58579440dccf8957b3f3cb6e20d
margonaut <margaret.morgane@gmail.com>
2016-02-28 14:16:33 -0500

    Added precompile statement for another bootsy gif

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( bootsy/gallery-loader.gif )
+Rails.application.config.assets.precompile += %w( bootsy/upload-loader.gif )
